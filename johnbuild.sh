e4b22700c105c84df26b7ea7e7f61c6098f2322f
BuildTools <unconfigured@null.spigotmc.org>
2025-11-25 18:00:31 -0500

    build file

--- johnbuild.sh
+++ johnbuild.sh
@@ -17,6 +17,8 @@ umask 022
 zstd --version
 ls -al staging_dir/host/bin/
 echo $PATH
+make tools/zstd/compile
+ls -al staging_dir/host/bin/
 make package/qca-mcs/{clean,prepare,compile} V=s
 
 #make download -j$(nproc)
